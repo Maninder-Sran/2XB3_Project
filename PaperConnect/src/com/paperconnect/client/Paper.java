b7b73947b17a5286068fbc57b40a1bd2149c8764
Maninder Sran <Maninder Sran@172.17.58.112>
2019-03-23 19:14:27 -0400

    Cleanup

--- PaperConnect/src/com/paperconnect/client/Paper.java
+++ PaperConnect/src/com/paperconnect/client/Paper.java
@@ -4,34 +4,32 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 //An ADT used to perform sorting of papers by citation count for the keywordLookup
-public class Paper implements Comparable<Paper>, Serializable{
-	
+public class Paper implements Comparable<Paper>, Serializable {
+
 	private long citeNum;
 	private ArrayList<String> references;
 	private ArrayList<String> fields;
 
-	
-	public enum Fields {
-		ID,
-		TITLE,
-		AUTHOR,
-		PUBLISH_DATE,
-		REFERENCES,
-		ABSTRACT;
+	public enum Fields implements Serializable {
+		ID, TITLE, AUTHOR, PUBLISH_DATE, REFERENCES, ABSTRACT;
 	}
-	
-	
-	public Paper() {}
-	
-	//Constructor
+
+	public Paper() {
+		fields = new ArrayList<String>();
+	}
+
+	// Constructor
 	public Paper(String paperID, String paperTitle, long citeNum) {
+		fields = new ArrayList<String>();
 		addField(paperID);
 		addField(paperTitle);
 		this.citeNum = citeNum;
 	}
-	
-	//Constructor
-	public Paper(String paperID, String paperTitle, String paperAbstract, ArrayList<String> references, String paperAuthor, String publishDate,long citeNum) {
+
+	// Constructor
+	public Paper(String paperID, String paperTitle, String paperAbstract, ArrayList<String> references,
+			String paperAuthor, String publishDate, long citeNum) {
+		fields = new ArrayList<String>();
 		addField(paperID);
 		addField(paperTitle);
 		addField(paperAuthor);
@@ -40,7 +38,7 @@ public class Paper implements Comparable<Paper>, Serializable{
 		this.references = references;
 		this.citeNum = citeNum;
 	}
-	
+
 	public void addField(String a) {
 		fields.add(a);
 	}
@@ -48,20 +46,20 @@ public class Paper implements Comparable<Paper>, Serializable{
 	public String getField(Fields field) {
 		return fields.get(field.ordinal());
 	}
-	
-	public ArrayList<String> getReferences(){
+
+	public ArrayList<String> getReferences() {
 		return references;
 	}
-	
+
 	public String toString() {
 		return getField(Fields.ID) + ", " + getField(Fields.TITLE) + "\n" + getField(Fields.ABSTRACT);
 	}
-	
+
 	@Override
 	public int compareTo(Paper j) {
-		if(citeNum > j.citeNum)
+		if (citeNum > j.citeNum)
 			return -1;
-		else if(citeNum < j.citeNum)
+		else if (citeNum < j.citeNum)
 			return 1;
 		return 0;
 	}
--- PaperConnect/src/com/paperconnect/server/DataServer.java
+++ PaperConnect/src/com/paperconnect/server/DataServer.java
@@ -15,14 +15,13 @@ import org.json.simple.parser.ParseException;
 import com.paperconnect.client.LookupTableLine;
 import com.paperconnect.client.Paper;
 import com.paperconnect.client.PaperShort;
-import com.paperconnect.server.DataServer.PaperList.LookupTable;
 import com.paperconnect.util.Search;
 
 public class DataServer {
 
 	public static void init() {
-		PaperList.init();
 		LookupTable.init();
+		PaperList.init();
 	}
 
 	public static class PaperList {
@@ -35,7 +34,6 @@ public class DataServer {
 		private static void readPaperFile(String fileName) {
 			String line = null, id, title, abst, author, publishDate;
 			long citations;
-			ArrayList<String> references = new ArrayList<String>();
 			JSONObject obj;
 			JSONArray temp;
 			papers = new ArrayList<Paper>();
@@ -54,9 +52,13 @@ public class DataServer {
 
 					// Get title of paper
 					title = obj.get("title").toString();
-					if (title == null || title.contains("??"))
+					if (title == null || title.contains("??")) {
+						try {
 						title = obj.get("venue").toString();
-
+						} catch (NullPointerException e) {
+							title = "Generic paper";
+						}
+					}
 					// Get abstract of paper
 					try {
 						abst = obj.get("abstract").toString();
@@ -72,11 +74,17 @@ public class DataServer {
 						citations = 0;
 					}
 
-					temp = (JSONArray) obj.get("references");
-					// get all references of paper
-					Iterator<String> iterator = temp.iterator();
-					while (iterator.hasNext()) {
-						references.add(iterator.next());
+					ArrayList<String> references = new ArrayList<String>();
+
+					try {
+						temp = (JSONArray) obj.get("references");
+						// get all references of paper
+						Iterator<String> iterator = temp.iterator();
+						while (iterator.hasNext()) {
+							references.add(iterator.next());
+						}
+					} catch (NullPointerException e) {
+
 					}
 
 					// Get author of paper
@@ -93,10 +101,10 @@ public class DataServer {
 					} catch (NullPointerException e) {
 						publishDate = "NA";
 					}
-
 					// Load paper data into PaperADT and store it in list of PaperADTs
 					paper = new Paper(id, title, abst, references, author, publishDate, citations);
 					papers.add(paper);
+					obj = null;
 				}
 			} catch (
 
@@ -110,74 +118,76 @@ public class DataServer {
 			}
 		}
 
-		public static class LookupTable {
-			static ArrayList<LookupTableLine> lookupTable;
+	}
 
-			public static void init() {
-				readLookupTable("data/ap_0_lookup_sorted.txt");
-				/*
-				 * for (LookupTableLine l : lookupTable) { System.out.println(l); }
-				 */
-			}
+	public static class LookupTable {
+		static ArrayList<LookupTableLine> lookupTable;
 
-			private static void readLookupTable(String fileName) {
-				String line = null;
-				lookupTable = new ArrayList<LookupTableLine>();
-				long start = System.nanoTime();
-				int count = 0;
-				// used for splitting by "="
-				String[] lineSplit = null;
-				// used for splitting by ","
-				String[] lineSplit2 = null;
-				LookupTableLine tableLine = null;
-				try {
-					FileReader fileReader = new FileReader(fileName);
-					BufferedReader bufferedReader = new BufferedReader(fileReader);
-					while (true) {
-						try {
-							line = bufferedReader.readLine();
-							if (line == null) {
-								break;
-							}
-							lineSplit = line.split("=");
-							lineSplit[0] = lineSplit[0].trim();
-							lineSplit[1] = lineSplit[1].trim();
-							// set the keyword
-							tableLine = new LookupTableLine(lineSplit[0]);
-							// remove []
-							lineSplit[1] = lineSplit[1].substring(1, lineSplit[1].length() - 1);
-							lineSplit = lineSplit[1].split(",");
-							for (int i = 0; i < lineSplit.length; i++) {
-								lineSplit2 = lineSplit[i].split("::");
-								tableLine.addPaperData(new String[] { lineSplit2[0].trim(), lineSplit2[1].trim() });
-							}
-							lookupTable.add(tableLine);
-							count++;
-						} catch (Exception f) {
-							continue;
+		public static void init() {
+			readLookupTable("data/ap_lookup.txt");
+			/*
+			 * for (LookupTableLine l : lookupTable) { System.out.println(l); }
+			 */
+		}
+
+		private static void readLookupTable(String fileName) {
+			String line = null;
+			lookupTable = new ArrayList<LookupTableLine>();
+			long start = System.nanoTime();
+			int count = 0;
+			// used for splitting by "="
+			String[] lineSplit = null;
+			// used for splitting by ","
+			String[] lineSplit2 = null;
+			LookupTableLine tableLine = null;
+			try {
+				FileReader fileReader = new FileReader(fileName);
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
+				while (true) {
+					try {
+						line = bufferedReader.readLine();
+						if (line == null) {
+							break;
 						}
+						lineSplit = line.split("=");
+						lineSplit[0] = lineSplit[0].trim();
+						lineSplit[1] = lineSplit[1].trim();
+						// set the keyword
+						tableLine = new LookupTableLine(lineSplit[0]);
+						// remove []
+						lineSplit[1] = lineSplit[1].substring(1, lineSplit[1].length() - 1);
+						lineSplit = lineSplit[1].split(",");
+						for (int i = 0; i < lineSplit.length; i++) {
+							lineSplit2 = lineSplit[i].split("::");
+							tableLine.addPaperData(new String[] { lineSplit2[0].trim(), lineSplit2[1].trim() });
+						}
+						lookupTable.add(tableLine);
+						count++;
+					} catch (Exception f) {
+						continue;
 					}
-					System.out.println("Done in " + (System.nanoTime() - start) / 1000000000.0 + " seconds. " + (count)
-							+ " nodes.");
-					bufferedReader.close();
-				} catch (FileNotFoundException e) {
-					e.printStackTrace();
-				} catch (IOException e) {
-					e.printStackTrace();
 				}
-			}
-
-			public static boolean isKeywordValid(String keyword) {
-				return retrievePapers(keyword) != null;
-			}
-
-			public static ArrayList<PaperShort> retrievePapers(String keyword) {
-				LookupTableLine result = Search.binarySearchKeyword(lookupTable, keyword);
-				if (result == null) {
-					return null;
-				}
-				return result.getData();
+				System.out.println(
+						"Done in " + (System.nanoTime() - start) / 1000000000.0 + " seconds. " + (count) + " nodes.");
+				bufferedReader.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
+
+		public static boolean isKeywordValid(String keyword) {
+			return retrievePapers(keyword) != null;
+		}
+
+		public static ArrayList<PaperShort> retrievePapers(String keyword) {
+			LookupTableLine result = Search.binarySearchKeyword(lookupTable, keyword);
+			if (result == null) {
+				return null;
+			}
+			return result.getData();
+		}
 	}
+
 }
