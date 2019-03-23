e07b576d5277f3f5847e78bdc0f461deb2874844
Purv <Purv@172.17.61.104>
2019-03-23 11:33:12 -0400

    pushing minor changes

--- PaperConnect/src/com/paperconnect/server/DataServer.java
+++ PaperConnect/src/com/paperconnect/server/DataServer.java
@@ -94,74 +94,74 @@ public class DataServer {
 
 	}
 
-public static class LookupTable {
-	static ArrayList<LookupTableLine> lookupTable;
+	public static class LookupTable {
+		static ArrayList<LookupTableLine> lookupTable;
 
-	public static void init() {
-		readLookupTable("data/ap_0_lookup_sorted.txt");
-		/*
-		 * for (LookupTableLine l : lookupTable) { System.out.println(l); }
-		 */
-	}
+		public static void init() {
+			readLookupTable("data/ap_0_lookup_sorted.txt");
+			/*
+			 * for (LookupTableLine l : lookupTable) { System.out.println(l); }
+			 */
+		}
 
-	private static void readLookupTable(String fileName) {
-		String line = null;
-		lookupTable = new ArrayList<LookupTableLine>();
-		long start = System.nanoTime();
-		int count = 0;
-		// used for splitting by "="
-		String[] lineSplit = null;
-		// used for splitting by ","
-		String[] lineSplit2 = null;
-		LookupTableLine tableLine = null;
-		try {
-			FileReader fileReader = new FileReader(fileName);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			while (true) {
-				try {
-					line = bufferedReader.readLine();
-					if (line == null) {
-						break;
+		private static void readLookupTable(String fileName) {
+			String line = null;
+			lookupTable = new ArrayList<LookupTableLine>();
+			long start = System.nanoTime();
+			int count = 0;
+			// used for splitting by "="
+			String[] lineSplit = null;
+			// used for splitting by ","
+			String[] lineSplit2 = null;
+			LookupTableLine tableLine = null;
+			try {
+				FileReader fileReader = new FileReader(fileName);
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
+				while (true) {
+					try {
+						line = bufferedReader.readLine();
+						if (line == null) {
+							break;
+						}
+						lineSplit = line.split("=");
+						lineSplit[0] = lineSplit[0].trim();
+						lineSplit[1] = lineSplit[1].trim();
+						// set the keyword
+						tableLine = new LookupTableLine(lineSplit[0]);
+						// remove []
+						lineSplit[1] = lineSplit[1].substring(1, lineSplit[1].length() - 1);
+						lineSplit = lineSplit[1].split(",");
+						for (int i = 0; i < lineSplit.length; i++) {
+							lineSplit2 = lineSplit[i].split("::");
+							tableLine.addPaperData(new String[] { lineSplit2[0].trim(), lineSplit2[1].trim() });
+						}
+						lookupTable.add(tableLine);
+						count++;
+					} catch (Exception f) {
+						continue;
 					}
-					lineSplit = line.split("=");
-					lineSplit[0] = lineSplit[0].trim();
-					lineSplit[1] = lineSplit[1].trim();
-					// set the keyword
-					tableLine = new LookupTableLine(lineSplit[0]);
-					// remove []
-					lineSplit[1] = lineSplit[1].substring(1, lineSplit[1].length() - 1);
-					lineSplit = lineSplit[1].split(",");
-					for (int i = 0; i < lineSplit.length; i++) {
-						lineSplit2 = lineSplit[i].split("::");
-						tableLine.addPaperData(new String[] { lineSplit2[0].trim(), lineSplit2[1].trim() });
-					}
-					lookupTable.add(tableLine);
-					count++;
-				} catch (Exception f) {
-					continue;
 				}
+				System.out.println(
+						"Done in " + (System.nanoTime() - start) / 1000000000.0 + " seconds. " + (count) + " nodes.");
+				bufferedReader.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			System.out.println(
-					"Done in " + (System.nanoTime() - start) / 1000000000.0 + " seconds. " + (count) + " nodes.");
-			bufferedReader.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
-	}
 
-	public static boolean isKeywordValid(String keyword) {
-		return retrievePapers(keyword) != null;
-	}
-
-	public static ArrayList<PaperShort> retrievePapers(String keyword) {
-		LookupTableLine result = Search.binarySearchKeyword(lookupTable, keyword);
-		if (result == null) {
-			return null;
+		public static boolean isKeywordValid(String keyword) {
+			return retrievePapers(keyword) != null;
 		}
-		return result.getData();
-	}
+
+		public static ArrayList<PaperShort> retrievePapers(String keyword) {
+			LookupTableLine result = Search.binarySearchKeyword(lookupTable, keyword);
+			if (result == null) {
+				return null;
+			}
+			return result.getData();
+		}
 
 	}
 
