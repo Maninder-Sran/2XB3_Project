f709bf52112d66c446759143de212a35772a3730
Vanshaj Verma <vermav2@mcmaster.ca>
2019-03-23 20:41:56 -0400

    changed name of old bfs to avoid confusion

--- PaperConnect/src/com/paperconnect/graph/BreadthFirstSearch_old.java
+++ PaperConnect/src/com/paperconnect/graph/BreadthFirstSearch_old.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
-public class BreadthFirstSearch1 {
+public class BreadthFirstSearch_old {
 	public static void main(String[] args) throws FileNotFoundException {
 
 		File file = new File("citeGraph");
