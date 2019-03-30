64c013f410a67dae7472682510e28aabc2249874
jorda <jorda@172.17.62.196>
2019-03-30 12:35:49 -0400

    Updated DFS

--- PaperConnect/src/com/paperconnect/graph/DepthFirstSearch.java
+++ PaperConnect/src/com/paperconnect/graph/DepthFirstSearch.java
@@ -22,24 +22,32 @@ public class DepthFirstSearch {
 	public static String compute(DiGraph graph) {
 		Paper v;
 		ArrayList<Paper> neighbors = new ArrayList<Paper>();
-		String results = "";
+		String results = "[";
 		Stack<Paper> S = new Stack<Paper>();
 		S.push(startNode);
 		startNode.setVisited(true);
+		results += "[\n{";
+		boolean isDeadEnd = true;
 		while(!S.isEmpty()) {
 			v = S.pop();
-			System.out.println(v.getField(Fields.ID));
+			//System.out.println(v.getField(Fields.ID));
 			neighbors = graph.getChildren(v.getField(Fields.ID));
-			for(int i=0; i<neighbors.size(); i++) {
+			isDeadEnd = true;
+			
+			for(int i=0; i<Math.min(maxChildren, neighbors.size()); i++) {
 				if (neighbors.get(i).getVisited() != true) {
+					isDeadEnd = false;
 					S.push(neighbors.get(i));
 					neighbors.get(i).setVisited(true);
-					
+					//System.out.println(neighbors.get(i).getField(Fields.ID));
 				}
+			}
+			if(isDeadEnd) {
+				results += "\n}";
 			}
 		}
 		
-		
+		results += "\n}\n]";
 		return results;
 	}
 
@@ -51,6 +59,7 @@ public class DepthFirstSearch {
 		Paper p3 = new Paper("p3", "p3", 3);
 		Paper p4 = new Paper("p4", "p4", 4);
 		Paper p5 = new Paper("p5", "p5", 5);
+		Paper p6 = new Paper("p6", "p6", 6);
 		DiGraph d = new DiGraph(root);
 		d.addVertex("root");
 		d.addVertex("p1");
@@ -58,11 +67,13 @@ public class DepthFirstSearch {
 		d.addVertex("p3");
 		d.addVertex("p4");
 		d.addVertex("p5");
+		d.addVertex("p6");
 		d.addCiteEdge("root", p1);
 		d.addCiteEdge("root", p2);
 		d.addCiteEdge("root", p3);
 		d.addCiteEdge("root", p4);
 		d.addCiteEdge("p2", p5);
+		d.addCiteEdge("root", p6);
 		init(root, 3, 3);
 		String ans = compute(d);
 		System.out.println(ans);
