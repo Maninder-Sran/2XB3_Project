f10d4dea6b24ea3eb5974f76bd0d3a47ab9f5ceb
jorda <jorda@172.17.248.104>
2019-04-13 16:28:21 -0400

    Javadoc comments

--- PaperConnect/src/com/paperconnect/util/Search.java
+++ PaperConnect/src/com/paperconnect/util/Search.java
@@ -7,16 +7,15 @@ import com.paperconnect.client.Paper;
 import com.paperconnect.client.PaperFields;
 
 /**
- * @author Jordan Levy
  * Allows for data to be searched through
  */
 public class Search {
 
 	/**
+	 * Searches through a list of papers for an ID using binary search
 	 * @param list ArrayList<Paper> representing the list of papers to be searched through
 	 * @param id String representing the paper ID to look for
 	 * @return Paper object representing the Paper within list with a matching ID, or null if not found
-	 * Searches through a list of papers for an ID using binary search
 	 */
 	public static Paper binarySearchID(ArrayList<Paper> list, String id) {
 		int min = 0;
@@ -37,6 +36,12 @@ public class Search {
 		return null;
 	}
 
+	/**
+	 * Searches through a list of papers for an ID using sequential search
+	 * @param list ArrayList<Paper> representing the list of papers to be searched through
+	 * @param id String representing the paper ID to look for
+	 * @return Paper object representing the Paper within list with a matching ID, or null if not found
+	 */
 	public static Paper sequentialSearchID(ArrayList<Paper> list, String id) {
 		for (Paper p : list) {
 			if (p.getField(PaperFields.ID).equals(id)) {
@@ -46,6 +51,12 @@ public class Search {
 		return null;
 	}
 
+	/**
+	 * Searches through a lookup table for a keyword using binary search
+	 * @param list ArrayList<LookupTableLine> representing the lookup table to be searched through
+	 * @param keyword String representing the key
+	 * @return LookupTableLine object representing the corresponding value to the keyword, or null if not found
+	 */
 	public static LookupTableLine binarySearchKeyword(ArrayList<LookupTableLine> list, String keyword) {
 		int min = 0;
 		int max = list.size() - 1;
